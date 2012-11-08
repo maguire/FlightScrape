f83b370593425ad8b86697beeba214f2ebaf2067
maguire <maguire@>
2012-11-07 21:10:51 -0800

    findBestPrice.js add. Pulls best price data from google with hardcoded values

---
+++ findBestPrice.js
@@ -0,0 +1,67 @@
+var FlightResponseParser = {
+    parse : function(json) {
+
+        var response = eval(json);
+        var flightInfo = eval(response[1][0][2])
+        return flightInfo;
+    },
+
+    getDailyBestPrices : function(flights) {
+        sortedByDate = flights[1].sort();
+        datePrices = [];
+        for (var x = 0; x < sortedByDate.length; x++) {
+            datePrices[x] = {'date' : sortedByDate[x][1], 'price' : sortedByDate[x][3] }
+        } 
+        return datePrices;
+    }
+}
+
+var from = 'SEA'
+var to = 'ROC'
+var beginDate = '2012-11-09'
+var endDate = '2013-01-09'
+
+var http = require('http');
+var d = []
+d[0] = ''
+d[1] = '[,[[,"ca","[,[,,,,,,,,,,,,,,,,,,[[,[\\"'
+d[2] = from
+d[3] = '\\"]\\n,[\\"'
+d[4] = to
+d[5] = '\\"]\\n,\\"\\"]\\n]\\n]\\n,\\"'
+d[6] = beginDate //2012-11-09
+d[7] = '\\",\\"'
+d[8] = endDate //2012-12-09
+d[9] = '\\"]\\n",,4]],[,[[,"b_al","ld:135"],[,"b_am","ca"],[,"b_qu","0"],[,"b_qc","4"]]]]' 
+var reqData = d.join('');
+
+var options = {
+  host: 'google.com',
+  path: '/flights/rpc',
+  method: 'POST',
+  headers: {'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': reqData.length,
+            'Accept' : '*/*',
+            'X-GWT-Permutation':'AA0BE2EDE18DC4F0C784922828B2BA9F'} 
+};
+
+callback = function(response) {
+
+  var str = '';
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    flightObj = FlightResponseParser.parse(str);
+    a = FlightResponseParser.getDailyBestPrices(flightObj)
+    console.log(a)
+  });
+}
+
+var request = http.request(options, callback);
+request.end(reqData);
+
+
